f1f02aeec920d2a877e44517a3f94cc5f6ccfe63
Benedikt Beckermann <hello@benedikt.click>
2022-06-21 19:06:52 +0200

    Parallelised the programm in a second dimension

--- cgol.py
+++ cgol.py
@@ -5,16 +5,26 @@ import numpy as np
 # CELLS
 cells = np.array(
     [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ], 
     np.int32)
 
@@ -33,35 +43,39 @@ int checkField(__global uint *cells, int x, int y, int size){
     x = (x + size) % size;
     y = (y + size) % size;
 
-    if (cells[x*10+y] > 0)  return 1;
+    if (cells[x*size+y] > 0)  return 1;
     else                    return 0;
 }
 
-__kernel void demo(__global uint *cells_in, __global uint *cells_out, int size){
+__kernel void cgol(__global uint *cells_in, __global uint *cells_out, int size){
     int x = get_global_id(0);
-    for(int y=0; y<size; y++) {
-        uint neighbours = 0;
-        int isAlive = cells_in[x*10+y];
-        for (int i = -1; i<2; i++){
-            for (int j = -1; j<2; j++){
-                if(i!=0 || j!=0){
-                    neighbours += checkField(cells_in, x+i, y+j, size);
-                }
+    int y = get_global_id(1);
+    
+    uint neighbours = 0;
+    int isAlive = cells_in[x*size+y];
+    for (int i = -1; i<2; i++){
+        for (int j = -1; j<2; j++){
+            if(i!=0 || j!=0){
+                neighbours += checkField(cells_in, x+i, y+j, size);
             }
         }
-
-        //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        if(isAlive > 0 && neighbours < 2) isAlive = 0;
-        //Any live cell with two or three live neighbours lives on to the next generation.
-        else if(isAlive > 0 && neighbours < 4) isAlive = 1;
-        //Any live cell with more than three live neighbours dies, as if by overpopulation.
-        else if(isAlive > 0 && neighbours > 3) isAlive = 0;
-        //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-        else if(isAlive == 0 && neighbours == 3)isAlive = 1;
-
-        cells_out[x*10+y] = (uint) isAlive;
     }
-}""")
+
+    //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    if(isAlive > 0 && neighbours < 2) isAlive = 0;
+
+    //Any live cell with two or three live neighbours lives on to the next generation.
+    else if(isAlive > 0 && neighbours < 4) isAlive = 1;
+
+    //Any live cell with more than three live neighbours dies, as if by overpopulation.
+    else if(isAlive > 0 && neighbours > 3) isAlive = 0;
+
+    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    else if(isAlive == 0 && neighbours == 3)isAlive = 1;
+
+    cells_out[x*size+y] = (uint) isAlive;
+}
+""")
 
 
 try:
@@ -72,10 +86,10 @@ except Exception:
     raise
 
 
-for i in range(10):
-    prg.demo(queue, (len(cells),), None, cells_buffer, cells_buffer_out, np.int32(len(cells)))
+for i in range(10_000):
+    prg.cgol(queue, (len(cells),len(cells)), None, cells_buffer, cells_buffer_out, np.int32(len(cells)))
     cells_buffer_out, cells_buffer = cells_buffer, cells_buffer_out
-    cl.enqueue_copy(queue, cells, cells_buffer).wait()
-    print(f"\nGeneration {i}")
-    for res in cells:
-        print(res)
+cl.enqueue_copy(queue, cells, cells_buffer).wait()
+print("Last Generation")
+for res in cells:
+    print(res)
