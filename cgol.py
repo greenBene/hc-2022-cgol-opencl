e17bd5fd3e86bc0925fd119e6faed14083fedffc
Benedikt Beckermann <hello@benedikt.click>
2022-06-26 13:13:12 +0200

    Made it into a class

--- cgol.py
+++ cgol.py
@@ -1,95 +1,101 @@
 # Import required libraries 
 import pyopencl as cl
 import numpy as np
-
+# from matplotlib import pyplot as plt
+import time
 # CELLS
-cells = np.array(
-    [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ], 
-    np.int32)
 
-# Prepare OpenCL context
-ctx = cl.create_some_context()
-queue = cl.CommandQueue(ctx)
 
-mf = cl.mem_flags
-cells_buffer = cl.Buffer(ctx, mf.READ_WRITE, cells.nbytes)
-cells_buffer_out = cl.Buffer(ctx, mf.READ_WRITE, cells.nbytes)
-cl.enqueue_copy(queue, cells_buffer, cells).wait()
+class CGOL():
+    kernel = r"""
+        int checkField(__global int *cells, int x, int y, int size){
+            x = (x + size) % size;
+            y = (y + size) % size;
 
-prg = cl.Program(ctx,
-r"""
-int checkField(__global uint *cells, int x, int y, int size){
-    x = (x + size) % size;
-    y = (y + size) % size;
-
-    if (cells[x*size+y] > 0)  return 1;
-    else                    return 0;
-}
-
-__kernel void cgol(__global uint *cells_in, __global uint *cells_out, int size){
-    int x = get_global_id(0);
-    int y = get_global_id(1);
-    
-    uint neighbours = 0;
-    int isAlive = cells_in[x*size+y];
-    for (int i = -1; i<2; i++){
-        for (int j = -1; j<2; j++){
-            if(i!=0 || j!=0){
-                neighbours += checkField(cells_in, x+i, y+j, size);
-            }
+            if (cells[x*size+y] > 0)  return 1;
+            else                    return 0;
         }
-    }
 
-    //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    if(isAlive > 0 && neighbours < 2) isAlive = 0;
+        __kernel void cgol(__global int *cells_in, __global int *cells_out, int size){
+            int x = get_global_id(0);
+            int y = get_global_id(1);
+            
+            int neighbours = 0;
+            int isAlive = cells_in[x*size+y];
+            for (int i = -1; i<2; i++){
+                for (int j = -1; j<2; j++){
+                    if(i!=0 || j!=0){
+                        neighbours += checkField(cells_in, x+i, y+j, size);
+                    }
+                }
+            }
 
-    //Any live cell with two or three live neighbours lives on to the next generation.
-    else if(isAlive > 0 && neighbours < 4) isAlive = 1;
+            //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+            if(isAlive > 0 && neighbours < 2) isAlive = 0;
 
-    //Any live cell with more than three live neighbours dies, as if by overpopulation.
-    else if(isAlive > 0 && neighbours > 3) isAlive = 0;
+            //Any live cell with two or three live neighbours lives on to the next generation.
+            else if(isAlive > 0 && neighbours < 4) isAlive = 1;
 
-    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-    else if(isAlive == 0 && neighbours == 3)isAlive = 1;
+            //Any live cell with more than three live neighbours dies, as if by overpopulation.
+            else if(isAlive > 0 && neighbours > 3) isAlive = 0;
 
-    cells_out[x*size+y] = (uint) isAlive;
-}
-""")
+            //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            else if(isAlive == 0 && neighbours == 3)isAlive = 1;
 
-
-try:
-    prg.build()
-except Exception:
-    print("Error:")
-    print(prg.get_build_info(ctx.devices[0], cl.program_build_info.LOG))
-    raise
+            cells_out[x*size+y] = (int) isAlive;
+        }
+        """
 
 
-for i in range(5_000):
-    prg.cgol(queue, (len(cells),len(cells)), None, cells_buffer, cells_buffer_out, np.int32(len(cells)))
-    cells_buffer_out, cells_buffer = cells_buffer, cells_buffer_out
-cl.enqueue_copy(queue, cells, cells_buffer).wait()
-print("Last Generation")
-for res in cells:
-    print(res)
+    def __init__(self, size):
+        self.cells = np.random.randint(low=2, size=(size,size), dtype=np.int32)
+        self.size = size
+        # Prep OpelCL
+        self.ctx = cl.create_some_context()
+        self.queue = cl.CommandQueue(self.ctx)
+
+        mf = cl.mem_flags
+        self.cells_buffer = cl.Buffer(self.ctx, mf.READ_WRITE, self.cells.nbytes)
+        self.cells_buffer_out = cl.Buffer(self.ctx, mf.READ_WRITE, self.cells.nbytes)
+        cl.enqueue_copy(self.queue, self.cells_buffer, self.cells).wait()
+
+        self.prg = cl.Program(self.ctx, self.kernel)
+
+        try:
+            self.prg.build()
+        except Exception:
+            print("Error:")
+            print(self.prg.get_build_info(self.ctx.devices[0], cl.program_build_info.LOG))
+            raise
+    
+    def calculate_next_generation(self):
+        self.prg.cgol(
+            self.queue, 
+            (self.size,self.size), 
+            None, 
+            self.cells_buffer, 
+            self.cells_buffer_out, 
+            np.int32(self.size))
+        self.cells_buffer_out, self.cells_buffer = self.cells_buffer, self.cells_buffer_out
+
+    def get_cells(self):
+        cl.enqueue_copy(self.queue, self.cells, self.cells_buffer).wait()
+        return self.cells
+
+
+if __name__ == '__main__':
+    cgol = CGOL(5)
+
+
+    for i in range(20):
+        cgol.calculate_next_generation()
+        cells = cgol.get_cells()
+
+        print(f"{i}-th Generation")
+        s = ""
+        for row in cells:
+            for element in row:
+                s+= "X" if element == 1 else "O"
+            s+='\n'
+        print(s)
+        time.sleep(1)
